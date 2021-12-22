79df944fa76948ea02e2deeb92d1102b16ef6ba1
Islam <isxmad@gmail.com>
2021-12-22 20:14:12 +0300

    done

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,11 @@
+import ApexCharts from 'apexcharts';
+import MicroModal from 'micromodal';
+
 document.addEventListener('DOMContentLoaded', ()=> {
 
+
+    MicroModal.init();
+    
     function burgerMenu() {
         const burgerBtn = document.querySelector('.header__burger');
         const burgerMenu = document.querySelector('.menu');
@@ -48,6 +54,68 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     burgerMenu();
     asideMenu();
+
+    let options = {
+        chart: {
+          height: 280,
+          type: "area"
+        },
+        dataLabels: {
+          enabled: false
+        },
+        series: [
+          {
+            name: "Series 1",
+            data: [45, 52, 38, 45, 19, 23, 2]
+          },
+              {
+            name: "Series 2",
+            data: [25, 22, 32, 54, 12, 13, 2]
+          }
+        ],
+        fill: {
+          type: "gradient",
+          gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.7,
+            opacityTo: 0.9,
+            stops: [0, 90, 100]
+          }
+        },
+        xaxis: {
+          categories: [
+            "01 Jan",
+            "02 Jan",
+            "03 Jan",
+            "04 Jan",
+            "05 Jan",
+            "06 Jan",
+            "07 Jan"
+          ]
+        },
+        responsive: [{
+          breakpoint: 500,
+          options: {
+            chart: {
+              height: 200
+            }
+          }
+        }]
+    };
+      
+
+    const charts = document.querySelectorAll('.chartC');
+
+    charts.forEach(i => {
+        if(i) {
+            let chart = new ApexCharts(i, options);
+
+            chart.render();
+        } 
+    });
+
+
+
 
 
     /*Chart*/
